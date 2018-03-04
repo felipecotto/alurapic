311efa431d8e279a5471afcabaacf28457861c91
Felipe Cotto <felipeoliveira.contato@gmail.com>
2018-03-04 17:28:11 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="corpo"> 
+    
+    <meu-menu :rotas="routes"/>
+
+    <transition name="pagina-fade">
+        <router-view></router-view>
+    </transition>
+
+    </div>
+</template>
+
+<script>
+
+    import { routes } from './routes';
+    import Menu from './components/shared/menu/Menu.vue';
+
+    export default {
+
+        components: {
+            'meu-menu' : Menu
+        },
+
+        data() {
+            return {
+                routes
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+    .corpo {
+        font-family: Helvetica, sans-serif;
+        width: 96%;
+        margin: 0 auto;
+    }
+
+    .pagina-fade-enter, .pagina-fade-leave-active {
+      opacity: 0
+    }
+
+    .pagina-fade-enter-active, .pagina-fade-leave-active {
+      transition: opacity .4s
+    }
+
+</style>
---
+++ src/components/shared/imagem-responsiva/ImagemResponsiva.vue
@@ -0,0 +1,19 @@
+<template>
+	<img :src="url" :alt="titulo" class="imagem-responsiva">
+</template>
+
+<script>
+	export default {
+		props: ['url', 'titulo']
+	}
+</script>
+
+<style scoped>
+
+	.imagem-responsiva {
+		display: block;
+	    max-width: 100%;
+	    height: auto;
+	}
+
+</style>
